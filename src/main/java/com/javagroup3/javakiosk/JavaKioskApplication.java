aa4c5cc20379566ece1e85437c2985cec42e3d60
hanuri5155 <hanuri5155@gmail.com>
2023-10-13 01:55:18 +0900

    1st commit

---
+++ src/main/java/com/javagroup3/javakiosk/JavaKioskApplication.java
@@ -0,0 +1,13 @@
+package com.javagroup3.javakiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKioskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javagroup3/javakiosk/JavaKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javagroup3.javakiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
