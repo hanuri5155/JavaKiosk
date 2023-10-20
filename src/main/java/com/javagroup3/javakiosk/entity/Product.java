584482cd756c7b92cabe770d4ae5c47496f8013d
20213846 <n5lp97@gmail.com>
2023-10-20 19:53:35 +0900

    feat: 데이터베이스 매핑 엔티티 작성

---
+++ src/main/java/com/javagroup3/javakiosk/entity/Product.java
@@ -0,0 +1,19 @@
+package com.javagroup3.javakiosk.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int product_id; // 회원 아이디
+
+    private String name; // 제품 이름
+    private int price; // 제품 가격
+    private boolean is_orderable; // 주문 가능 여부
+}
