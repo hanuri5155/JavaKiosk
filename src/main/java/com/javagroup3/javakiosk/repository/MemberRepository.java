a4bd1059dbf477e071b6d470873c1218910cbef4
20213846 <n5lp97@gmail.com>
2023-10-20 23:43:54 +0900

    refactor: 클래스 이름 변경
    User 를 Member 로 변경.

--- src/main/java/com/javagroup3/javakiosk/repository/MemberRepository.java
+++ src/main/java/com/javagroup3/javakiosk/repository/MemberRepository.java
@@ -1,12 +1,12 @@
 package com.javagroup3.javakiosk.repository;
 
-import com.javagroup3.javakiosk.entity.User;
+import com.javagroup3.javakiosk.entity.Member;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Transactional
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 }
