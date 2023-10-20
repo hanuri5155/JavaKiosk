c5a4898635344d6cbe74fd945b959d3c2d5512b2
20213846 <n5lp97@gmail.com>
2023-10-20 19:53:35 +0900

    feat: 데이터베이스 매핑 엔티티 작성

---
+++ src/main/java/com/javagroup3/javakiosk/entity/OrderRecord.java
@@ -0,0 +1,30 @@
+package com.javagroup3.javakiosk.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "order_record")
+public class OrderRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private int orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "order_quantity")
+    private int orderQuantity;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+}
