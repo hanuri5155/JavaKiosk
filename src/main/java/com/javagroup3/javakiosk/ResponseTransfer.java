d3f327272b12ebbdc9f747bd83d6260655bab0cb
20213846 <n5lp97@gmail.com>
2023-11-12 12:42:12 +0900

    Response 에 메시지를 담을 때 사용할 클래스

---
+++ src/main/java/com/javagroup3/javakiosk/ResponseTransfer.java
@@ -0,0 +1,12 @@
+package com.javagroup3.javakiosk;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseTransfer {
+    String message;
+}
