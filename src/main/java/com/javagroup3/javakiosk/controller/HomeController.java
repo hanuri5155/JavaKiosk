d0bcd9a33318ad8c1b0e223e327b96885ce0e5a2
hanuri5155 <hanuri5155@gmail.com>
2023-11-16 14:56:56 +0900

    test1

--- src/main/java/com/javagroup3/javakiosk/controller/HomeController.java
+++ src/main/java/com/javagroup3/javakiosk/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController { // wereqw
     // 메인 페이지 요청
     @GetMapping("/") // 메인 페이지 주소
     public String index(){
